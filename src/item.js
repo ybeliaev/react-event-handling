2e86c96e30dd2d2abf88b73d438d4258af2a2d50
Yura <ybeliaev@gmail.com>
2019-03-21 14:48:09 +0200

    upp

--- src/item.js
+++ src/item.js
@@ -4,8 +4,9 @@ const Item = ({ someProps }) => {
   // const onItemClick = ()=>console.log(123);
   const element = someProps.map(item => {
     const { id, name, age } = item;
+    const onItemClick = () => console.log(name);
     return (
-      <li key={id} onClick={() => console.log(name)}>
+      <li key={id} onClick={() => onItemClick()}>
         Name: {name}, age: {age}
       </li>
     );

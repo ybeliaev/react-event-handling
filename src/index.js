d2dfc27e68c0342910ead14718daaad49bc6f579
Yura <ybeliaev@gmail.com>
2019-03-21 11:30:55 +0200

    comm

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Event handling.</h1>
+      <h1>Event handling...</h1>
       <h2>Элементраная обработка событий.</h2>
     </div>
   );
